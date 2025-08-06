b19c4bd997f64c4f3939c6544b1c072bfaf6b5df
Crispin Manda <crispinmanda06@gmail.com>
2025-08-06 10:52:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+// This is a simple PHP script that outputs "Hello, World!" to the browser.
+echo 'Hello, World!';
+
+// variable declaration
+// Variables in PHP are used to store data that can be used later in the script.
+// We can declare variables in PHP using the $ sign followed by the variable name.
+$x = 5; // integer variable
+$y = 10; // integer variable
+$sum = $x + $y; // adding two integers
+// concatenation is the process of joining two strings together
+// In PHP, we can concatenate strings using the dot (.) operator.
+echo '<br>' .  $sum;
+$greeting = "Hello, PHP!"; // string variable
+
+
+?>
+
+
+    
+</body>
+</html>
